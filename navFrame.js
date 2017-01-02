3f91bce3b9830f4bd82718f41fa2df02e56b5ee1
RossHiga <ross@rosshiga.com>
2017-01-01 19:32:51 -1000

    Removed extra display, was causing edge case hiding of sidebar on desktop resize

--- navFrame.js
+++ navFrame.js
@@ -4,13 +4,13 @@
  * */
 
 function openNav() {
-    document.getElementById("navFrame").style.display = "block";
+
     document.getElementById("mobileNav").style.display = "none";
     document.getElementById("navFrame").className = "w3-col m5 l3 w3-container w3-collapse";
 
 }
 function closeNav() {
-    document.getElementById("navFrame").style.display = "none";
+
     document.getElementById("navFrame").className = "w3-col m5 l3 w3-container w3-collapse w3-hide-small";
     document.getElementById("mobileNav").style.display = "block";
 
